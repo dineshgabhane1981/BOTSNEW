1bea00036ddc7588b1604dd17e0fcff7a783cfee
Dinesh <dinesh.gabhane@hotmail.com>
2021-03-09 14:57:15 +0530

    PointsLedger files added

---
+++ Chitale/Controllers/PointsLedgerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chitale.Controllers
+{
+    public class PointsLedgerController : Controller
+    {
+        // GET: PointsLedger
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TgtVsAch()
+        {
+            return View();
+        }
+
+    }
+}
