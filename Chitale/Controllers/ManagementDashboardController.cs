7da47194b0e4fd35ce135e15220e929678ba9260
Dinesh <dinesh.gabhane@hotmail.com>
2021-03-15 17:11:30 +0530

    Added Dashboard Summery Code

--- Chitale/Controllers/ManagementDashboardController.cs
+++ Chitale/Controllers/ManagementDashboardController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using BOTS_BL;
+using BOTS_BL.Models.ChitaleModel;
+using BOTS_BL.Repository;
+using Chitale.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +13,40 @@ namespace Chitale.Controllers
 {
     public class ManagementDashboardController : Controller
     {
+        ManagementDashboardRepository MDR = new ManagementDashboardRepository();
+        Exceptions newexception = new Exceptions();
         // GET: ManagementDashboard
         public ActionResult Index()
         {
-            return View();
+            ManagementViewModel objModel = new ManagementViewModel();
+            objModel.ClusterList = MDR.GetClusterList();
+            objModel.SubClusterList = MDR.GetSubClusterList();
+            objModel.CityList = MDR.GetCityList();            
+            return View(objModel);
+        }
+        public JsonResult GetDashboardSummeryData(string Flag)
+        {
+            Dashboardsummary SummeryData = new Dashboardsummary();
+            try
+            {
+                var UserSession = (CustomerDetail)Session["ChitaleUser"];
+                bool IsBTD = false;
+                if (Flag == "1")
+                    IsBTD = true;
+                //SummeryData = CDR.GetSummeryDetails(UserSession.CustomerId, IsBTD);
+                SummeryData.PurchaseOrderPointsStr = String.Format(new CultureInfo("en-IN", false), "{0:n}", Convert.ToDecimal(SummeryData.PurchaseOrderPoints));
+                SummeryData.SalesOrderPointsStr = String.Format(new CultureInfo("en-IN", false), "{0:n}", Convert.ToDecimal(SummeryData.SalesOrderPoints));
+                SummeryData.RedeemedPointsStr = String.Format(new CultureInfo("en-IN", false), "{0:n}", Convert.ToDecimal(SummeryData.RedeemedPoints));
+                SummeryData.AddOnPointsStr = String.Format(new CultureInfo("en-IN", false), "{0:n}", Convert.ToDecimal(SummeryData.AddOnPoints));
+                SummeryData.LostPointsStr = String.Format(new CultureInfo("en-IN", false), "{0:n}", Convert.ToDecimal(SummeryData.LostPoints));
+                SummeryData.TotalPointsBalanceStr = String.Format(new CultureInfo("en-IN", false), "{0:n}", Convert.ToDecimal(SummeryData.TotalPointsBalance));
+
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex);
+            }
+            return new JsonResult() { Data = SummeryData, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
         }
         public ActionResult ParticipantList()
         {
---
+++ Chitale/ViewModel/ManagementViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chitale.ViewModel
+{
+    public class ManagementViewModel
+    {
+        public List<SelectListItem> ClusterList { get; set; }
+        public List<SelectListItem> SubClusterList { get; set; }
+        public List<SelectListItem> CityList { get; set; }
+    }
+}
