709b6419cf1f36f44c6db7ca2b3a866bef9cd166
Dinesh <dinesh.gabhane@hotmail.com>
2021-06-14 13:22:56 +0530

    Added New Mobile API Project

---
+++ BotsMobileAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BotsMobileAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BotsMobileAPI/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Owin.Security.Jwt;
+
+[assembly: OwinStartup(typeof(BotsMobileAPI.Startup))]
+
+namespace BotsMobileAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var url = System.Configuration.ConfigurationManager.AppSettings["BaseURL"];
+            app.UseJwtBearerAuthentication(
+                 new JwtBearerAuthenticationOptions
+                 {
+                     AuthenticationMode = AuthenticationMode.Active,
+                     TokenValidationParameters = new TokenValidationParameters()
+                     {
+                         ValidateIssuer = true,
+                         ValidateAudience = true,
+                         ValidateIssuerSigningKey = true,
+                         ValidIssuer = url, //some string, normally web url,  
+                         ValidAudience = url,
+                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my_secret_key_98765"))
+                     }
+                 });
+        }
+    }
+}
