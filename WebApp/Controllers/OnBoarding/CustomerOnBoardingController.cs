e82d2ff9a3aa35e9cd72089435ea5b2e941728c0
Dinesh <dinesh.gabhane@hotmail.com>
2021-07-14 22:46:33 +0530

    Onboarding Controller and Views Added

---
+++ WebApp/Controllers/OnBoarding/CustomerOnBoardingController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers.OnBoarding
+{
+    public class CustomerOnBoardingController : Controller
+    {
+        // GET: CustomerOnBoarding
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CustomerDetail()
+        {
+            return View();
+        }
+        public ActionResult ProductAssignmentDetails()
+        {
+            return View();
+        }
+        public ActionResult DealDetails()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentSchedule()
+        {
+            return View();
+        }
+
+        public ActionResult UpdatePaymentDetails()
+        {
+            return View();
+        }
+        public ActionResult Notifications()
+        {
+            return View();
+        }
+
+    }
+}
