c41ff1459023e1c5c5a8f20a48a554f85a35a861
Dinesh <dinesh.gabhane@hotmail.com>
2021-07-26 15:20:26 +0530

    CORS issue

--- BotsMobileAPI/App_Start/WebApiConfig.cs
+++ BotsMobileAPI/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using System.Web.Http.Cors;
+using System.Net.Http.Headers;
 
 namespace BotsMobileAPI
 {
@@ -11,10 +12,11 @@ namespace BotsMobileAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             // Web API configuration and services    
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-            //EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            //EnableCorsAttribute cors = new EnableCorsAttribute("https://blueocktopus.in/MobileNewAPI/", "*", "*");
             //config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
--- BotsMobileAPI/Global.asax.cs
+++ BotsMobileAPI/Global.asax.cs
@@ -19,5 +19,19 @@ namespace BotsMobileAPI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest()
+        {
+
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                //These headers are handling the "pre-flight" OPTIONS call sent by the browser
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "https://blueocktopus.in/MobileNewAPI/");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Accepts, Content-Type, Origin, X-My-Header");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "60");
+                HttpContext.Current.Response.End();
+            }
+
+        }
     }
 }
