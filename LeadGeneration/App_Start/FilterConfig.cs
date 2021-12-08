796510c7e2c3000e6baa9b883b74016cefd44afa
Dinesh <dinesh.gabhane@hotmail.com>
2021-12-08 19:09:12 +0530

    Timeout issue

--- LeadGeneration/App_Start/FilterConfig.cs
+++ LeadGeneration/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace LeadGeneration
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new MyActionFilterAttribute());
         }
     }
 }
---
+++ LeadGeneration/App_Start/SessionTimeOut.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeadGeneration
+{
+
+    public class MyActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var sessionUser = filterContext.HttpContext.Session["UserSession"];
+            var routeValues = filterContext.RequestContext.RouteData.Values["controller"].ToString();
+            string actionName = filterContext.RequestContext.RouteData.Values["action"].ToString();
+            if (sessionUser == null && !routeValues.Equals("Login"))
+            {
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    filterContext.HttpContext.Response.ClearContent();
+                    filterContext.HttpContext.Items["AjaxPermissionDenied"] = true;
+                }
+                else
+                {
+                    filterContext.Result = new RedirectResult("~/Login/Index");
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+
+
+
+}
