d8a60d2de50f12600662a14f6a11b367fe87fca5
Samiksha Wanjari <samiksha.wanjari@outlook.com>
2022-03-28 14:28:41 +0530

    Disable SMS Completed

--- WebApp/Controllers/ITOPS/SMSAndSecurityController.cs
+++ WebApp/Controllers/ITOPS/SMSAndSecurityController.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using BOTS_BL;
+using BOTS_BL.Models;
+using BOTS_BL.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace WebApp.Controllers.ITOPS
 {
     public class SMSAndSecurityController : Controller
     {
+
+        ITOpsRepository ITOPS = new ITOpsRepository();
+        ReportsRepository RR = new ReportsRepository();
+        CustomerRepository objCustRepo = new CustomerRepository();
+        Exceptions newexception = new Exceptions();
         // GET: SMSAndSecurity
         public ActionResult Index()
         {
@@ -17,6 +28,111 @@ namespace WebApp.Controllers.ITOPS
         public ActionResult SecurityKey()
         {
             return View();
+        }
+
+        public ActionResult GetChangeNameData(string MobileNo, string CardNo)
+        {
+            var GroupId = (string)Session["GroupId"];
+            MemberData objCustomerDetail = new MemberData();
+            if (!string.IsNullOrEmpty(MobileNo))
+            {
+                objCustomerDetail = ITOPS.GetChangeNameByMobileNo(GroupId, MobileNo);
+            }
+            if (!string.IsNullOrEmpty(CardNo))
+            {
+                objCustomerDetail = ITOPS.GetChangeNameByCardNo(GroupId, CardNo);
+            }
+
+            return Json(objCustomerDetail, JsonRequestBehavior.AllowGet);
+        }
+
+        public bool ChangeSMSDetails(string jsonData)
+        {
+            bool result = false;
+            string GroupId = "";
+            try
+            {
+                GroupId = (string)Session["GroupId"];
+                JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+                json_serializer.MaxJsonLength = int.MaxValue;
+                object[] objData = (object[])json_serializer.DeserializeObject(jsonData);
+                tblAudit objAudit = new tblAudit();
+                bool IsSMS = false;
+
+                string CustomerId = "";
+                bool DisableSMS = false;
+                foreach (Dictionary<string, object> item in objData)
+                {
+                    
+                    CustomerId = Convert.ToString(item["CustomerId"]);
+                    string Disable = Convert.ToString(item["Disable"]);
+                    if (Disable == "1")
+                        DisableSMS = true;
+                    
+                    objAudit.RequestedFor = "Change SMS setting";
+                    objAudit.RequestedEntity = "Change SMS setting for - " + CustomerId;
+                    objAudit.RequestedBy = Convert.ToString(item["RequestedBy"]);
+                    objAudit.RequestedOnForum = Convert.ToString(item["RequestedForum"]);
+                    objAudit.RequestedOn = Convert.ToDateTime(item["RequestedDate"]);
+
+                    IsSMS = Convert.ToBoolean(item["IsSMS"]);
+
+                }
+
+                result = ITOPS.ChangeSMSDetails(GroupId,CustomerId, DisableSMS, objAudit);
+                if (result)
+                {
+                    var subject = "New Customer Added with Mobile No  - " + CustomerId;
+                    var body = "New Customer Added with Mobile No - " + CustomerId;
+                    body += "<br/><br/> Regards <br/> Blue Ocktopus Team";
+
+                    SendEmail(GroupId, subject, body);
+                }
+                if (Convert.ToBoolean(IsSMS))
+                {
+                    //Logic to send SMS to Customer whose Name is changed
+                }
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, GroupId);
+            }
+            return result;
+        }
+
+        public void SendEmail(string GroupId, string Subject, string EmailBody)
+        {
+            var senderEmail = System.Configuration.ConfigurationManager.AppSettings["Email"];
+            var senderEmailPassword = System.Configuration.ConfigurationManager.AppSettings["EmailPassword"];
+
+            var email = new MailAddress(senderEmail, "Support Blue Ocktopus");
+            var toemail = ITOPS.GetCustomerAdminEmail(GroupId);
+            if (!string.IsNullOrEmpty(toemail))
+            {
+                var receiverEmail = new MailAddress(toemail);
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+
+                    Credentials = new NetworkCredential(email.Address, senderEmailPassword)
+                };
+                using (var mess = new MailMessage(email, receiverEmail)
+                {
+                    Subject = Subject,
+                    Body = EmailBody,
+                    IsBodyHtml = true,
+                    Priority = MailPriority.High,
+                    BodyEncoding = System.Text.Encoding.UTF8
+                })
+                {
+                    smtp.Send(mess);
+                }
+            }
         }
     }
 }
