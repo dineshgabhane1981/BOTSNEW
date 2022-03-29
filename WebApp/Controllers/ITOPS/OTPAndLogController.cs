1a0a771cdfc8b48f8880336e7e81bac774646dc4
sujith flowrin <sujithflowrin@gmail.com>
2022-03-29 16:26:44 +0530

    OtpAndLog/Logview

--- WebApp/Controllers/ITOPS/OTPAndLogController.cs
+++ WebApp/Controllers/ITOPS/OTPAndLogController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BOTS_BL;
+using BOTS_BL.Models;
+using BOTS_BL.Repository;
+using WebApp.App_Start;
 
 namespace WebApp.Controllers.ITOPS
 {
     public class OTPAndLogController : Controller
     {
         // GET: OTPAndLog
+        ITOpsRepository ITOPS = new ITOpsRepository();
+        ReportsRepository RR = new ReportsRepository();
+        CustomerRepository objCustRepo = new CustomerRepository();
+        Exceptions newexception = new Exceptions();
+        // GET: EarnBurn
+        string groupId;
         public ActionResult Index()
         {
             return View();
@@ -16,7 +26,35 @@ namespace WebApp.Controllers.ITOPS
 
         public ActionResult Log()
         {
+            try
+            {
+                CommonFunctions common = new CommonFunctions();
+                //groupId = common.DecryptString(groupId);
+                groupId = Session["GroupId"].ToString();
+                string connStr = objCustRepo.GetCustomerConnString(groupId);
+                var lstOutlet = RR.GetOutletList(groupId, connStr);
+                var lstBrand = RR.GetBrandList(groupId, connStr);
+                var GroupDetails = objCustRepo.GetGroupDetails(Convert.ToInt32(groupId));
+                ViewBag.OutletList = lstOutlet;
+                ViewBag.BranchList = lstBrand;
+                ViewBag.GroupId = groupId;
+
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, groupId);
+            }
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult GetTxnLogData(string GroupId, string search)
+        {
+            List<LogDetailsRW> lstLogDetails = new List<LogDetailsRW>();
+            ITOpsRepository ITOPS = new ITOpsRepository();
+            lstLogDetails = ITOPS.GetLogDetails(search, GroupId);
+            return Json(lstLogDetails, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
