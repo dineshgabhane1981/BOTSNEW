5108fab9dd7162a13ce634c994bc36f12691bd65
Samiksha Wanjari <samiksha.wanjari@outlook.com>
2022-03-29 12:58:12 +0530

    OTP Completed

--- WebApp/Controllers/ITOPS/OTPAndLogController.cs
+++ WebApp/Controllers/ITOPS/OTPAndLogController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BOTS_BL;
+using BOTS_BL.Models;
+using BOTS_BL.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +11,10 @@ namespace WebApp.Controllers.ITOPS
 {
     public class OTPAndLogController : Controller
     {
+        ITOpsRepository ITOPS = new ITOpsRepository();
+        ReportsRepository RR = new ReportsRepository();
+        CustomerRepository objCustRepo = new CustomerRepository();
+        Exceptions newexception = new Exceptions();
         // GET: OTPAndLog
         public ActionResult Index()
         {
@@ -17,6 +24,18 @@ namespace WebApp.Controllers.ITOPS
         public ActionResult Log()
         {
             return View();
+        }
+
+        public ActionResult GetOTPData(string MobileNo)
+        {
+            MemberData objCustomerDetail = new MemberData();
+            var GroupId = (string)Session["GroupId"];
+            if (!string.IsNullOrEmpty(MobileNo))
+            {
+                objCustomerDetail = ITOPS.GetOTPData(GroupId, MobileNo);
+            }
+
+            return Json(objCustomerDetail, JsonRequestBehavior.AllowGet);
         }
     }
 }
