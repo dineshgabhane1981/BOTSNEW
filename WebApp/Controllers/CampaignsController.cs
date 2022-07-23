5d1f53c665d938fe0bc1ecd58acf5114c70c1472
sujith flowrin <sujithflowrin@gmail.com>
2022-07-23 16:48:59 +0530

    CampaignTestSMS/CampControl,Createcamp

--- WebApp/Controllers/CampaignsController.cs
+++ WebApp/Controllers/CampaignsController.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using WebApp.ViewModel;
 
 namespace WebApp.Controllers
 {
@@ -297,14 +298,122 @@ namespace WebApp.Controllers
 
             return View("CampaignManagement");
         }
-        public ActionResult CampDLTList()
-        {
-            //var userDetails = (CustomerLoginDetail)Session["UserSession"];
-            //var ListCampDetails = CMPR.GetCampList(userDetails.GroupId, userDetails.connectionString);
-            //ViewBag.ListCampDetails = ListCampDetails;
 
-            return View("CampaignDLTList");
+        [HttpPost]
+        public JsonResult SendToDLT(string jsonData)
+        {
+            //SPResponse response = new SPResponse();
+            bool Status;
+            Status = false;
+            CampaignRepository CR = new CampaignRepository();
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            json_serializer.MaxJsonLength = int.MaxValue;
+            object[] objData = (object[])json_serializer.DeserializeObject(jsonData);
+            foreach (Dictionary<string, object> item in objData)
+            {
+                string CampaignId = Convert.ToString(item["CampaignId"]);
+
+               var response = CR.SendDLTData(CampaignId, userDetails.GroupId, userDetails.connectionString);
+                //Session["CampaignId"] = SaveData.;
+                Status = response;
+            }
+           
+            return new JsonResult() { Data = Status, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
         }
 
+        public ActionResult CampDLTList()
+        {
+            CampaignRepository CR = new CampaignRepository();
+            OnBoardingSalesViewModel objData = new OnBoardingSalesViewModel();
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
+            var lstDLTDetails = CR.CampDLTDetailsLst(userDetails.GroupId, userDetails.connectionString);
+            //  ViewBag.DLTDetails = lstDLTDetails;
+            ViewBag.TempleteType = objData.TempleteType();
+
+            return View("CampaignDLTList", lstDLTDetails);
+        }
+
+        public ActionResult UpdateCampDLCLinkDLTStatus(string Campid, string status, string reason)
+        {
+            //bool result = false;
+            CampaignRepository CR = new CampaignRepository();
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
+            var Response = CR.UpdateCampDLCLinkDLTStatus(Campid, status, reason, userDetails.GroupId, userDetails.connectionString);
+            return new JsonResult() { Data = Response, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+        }
+
+        public ActionResult UpdateCampDLTRejectStatus(string Campid, string status, string reason)
+        {
+            //bool result = false;
+            CampaignRepository CR = new CampaignRepository();
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
+            var Response = CR.UpdateCampDLTRejectStat(Campid, status, reason, userDetails.GroupId, userDetails.connectionString);
+            return new JsonResult() { Data = Response, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+        }
+
+        public ActionResult SaveCampDLCDetails(string Campid, string status, string jsonData)
+        {
+            bool result = false;
+            try
+            {
+                var userDetails = (CustomerLoginDetail)Session["UserSession"];
+                List<DLTDetailsLst> CampDetails = new List<DLTDetailsLst>();
+                CampaignRepository CR = new CampaignRepository();
+                JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+                json_serializer.MaxJsonLength = int.MaxValue;
+                object[] objData = (object[])json_serializer.DeserializeObject(jsonData);
+                foreach (Dictionary<string, object> item in objData)
+                {
+                    DLTDetailsLst CP = new DLTDetailsLst();
+                    if (CampDetails != null)
+                    {
+
+                        CP.Script = Convert.ToString(item["Script"]);
+                        CP.DLTScript = Convert.ToString(item["ScriptDLT"]);
+                        CP.TemplateID = Convert.ToString(item["TemplateId"]);
+                        CP.TemplateName = Convert.ToString(item["TemplateName"]);
+                        CP.TemplateType = Convert.ToString(item["TemplateType"]);
+                        if (Convert.ToString(status) == "Approved")
+                        {
+                            CP.DLTStatus = "Approved";
+                        }
+                        CampDetails.Add(CP);
+                        //objDLCLinkConfig.UpdatedBy = userDetails.LoginId;
+                        //objDLCLinkConfig.UpdatedDate = DateTime.Now;
+                    }
+                }
+                result = CR.SaveDLCCampaignDetails(Campid, CampDetails, userDetails.GroupId, userDetails.connectionString);
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "SaveCampDLCDetails");
+            }
+            return new JsonResult() { Data = result, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+        }
+
+        [HttpPost]
+        public JsonResult SendTestSMS(string jsonData)
+        {
+            //SPResponse response = new SPResponse();
+            //bool Status;
+            //Status = false;
+            CampaignRepository CR = new CampaignRepository();
+            List<CampaignSaveDetails> responsedata = new List<CampaignSaveDetails>();
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            json_serializer.MaxJsonLength = int.MaxValue;
+            object[] objData = (object[])json_serializer.DeserializeObject(jsonData);
+            foreach (Dictionary<string, object> item in objData)
+            {
+                string CampaignId = Convert.ToString(item["CampaignId"]);
+
+                responsedata = CR.SendTestSMSData(CampaignId, userDetails.GroupId, userDetails.connectionString);
+                //Session["CampaignId"] = SaveData.;
+                //responsedata = response;
+            }
+
+            return new JsonResult() { Data = responsedata, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+        }
     }
 }
