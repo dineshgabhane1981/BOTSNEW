01d38a4f42b248d1f456e87a22f3a0e2e79ab766
Nisha Wanjari <nishawanjari@outlook.com>
2022-09-06 11:58:36 +0530

    Npc Changes

--- BOTS_BL/Repository/NPCRepository.cs
+++ BOTS_BL/Repository/NPCRepository.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BOTS_BL.Models;
+using System.Data.Entity.Migrations;
 
 namespace BOTS_BL.Repository
 {
     public class NPCRepository
     {
+        public bool SaveNPCData(tblNPCDetail objOutletData)
+        {
+            bool result = false;
+            using (var context = new BOTSDBContext())
+            {
+                try
+                {                    
+                        context.tblNPCDetails.AddOrUpdate(objOutletData);
+                        context.SaveChanges();
+                        result = true;
+                     
+
+                }
+                catch (Exception ex)
+                {
+                    //newexception.AddException(ex, "SaveNPCData");
+
+                }
+            }
+            return result;
+        }
     }
+
 }
--- NPC/Controllers/LoginController.cs
+++ NPC/Controllers/LoginController.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BOTS_BL.Repository;
+using BOTS_BL.Models;
+using System.Web.Script.Serialization;
 
 namespace NPC.Controllers
 {
-    public class LoginController : Controller
+    public class Login1Controller : Controller
     {
+        NPCRepository NPCR = new NPCRepository();
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult SaveNPC()
+        public ActionResult SaveNPC(string jsonData)
         {
-            return View();
-        }
+            bool result = false;
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
 
-        
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            json_serializer.MaxJsonLength = int.MaxValue;
+            object[] objData = (object[])json_serializer.DeserializeObject(jsonData);
+            tblNPCDetail objOutletData = new tblNPCDetail();
+            foreach (Dictionary<string, object> item in objData)
+            {
+                
+                objOutletData.CustomerMobileNo = Convert.ToString(item["CustomerMobileNo"]);
+                objOutletData.CustomerName = Convert.ToString(item["CustomerName"]);
+                objOutletData.EmployeeName = Convert.ToString(item["EmployeeName"]);
+                objOutletData.CategoryName = Convert.ToString(item["CategoryName"]);
+                objOutletData.SubCategoryName = Convert.ToString(item["SubCategoryName"]);
+                objOutletData.NextVisitDay = Convert.ToDecimal(item["NextVisitDay"]);
+                objOutletData.Remarks = Convert.ToString(item["Remarks"]);
+                
+            }
+            result = NPCR.SaveNPCData(objOutletData);
+
+            return new JsonResult() { Data = result, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+
+        }
 
     }
 }
