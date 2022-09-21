29ce9e1d26114f18cf6ca28565d1d1bcdaf88569
Samiksha Wanjari <samiksha.wanjari@outlook.com>
2022-09-21 18:56:43 +0530

    Added Promotional pages

---
+++ WebApp/Controllers/BOPromoCampaignController.cs
@@ -0,0 +1,32 @@
+﻿using BOTS_BL.Models;
+using BOTS_BL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.ViewModel;
+
+namespace WebApp.Controllers
+{
+    public class BOPromoCampaignController : Controller
+    {
+        PromoCampaignRepository PCR = new PromoCampaignRepository();
+
+        // GET: BOPromoCampaign
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GroupList(string groupId)
+        {
+            PromoCampaignViewModel objData = new PromoCampaignViewModel();
+            objData.lstGroupList = PCR.GetGroupDetails();
+
+            return View(objData);
+        }
+
+        
+    }
+}
---
+++ WebApp/ViewModel/PromoCampaignViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BOTS_BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.ViewModel
+{
+    public class PromoCampaignViewModel
+    {
+        public List<tblGroupDetail> lstGroup { get; set; }
+        public List<SelectListItem> lstGroupList { get; set; }
+    }
+}
