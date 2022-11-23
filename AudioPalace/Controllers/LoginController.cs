f9b07b826d97ccf3deb4b21e68ceb7440aa52b49
Samiksha Wanjari <samiksha.wanjari@outlook.com>
2022-11-23 18:56:29 +0530

    Audio Palace Project

---
+++ AudioPalace/Controllers/LoginController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BOTS_BL.Models;
+using BOTS_BL;
+using BOTS_BL.Repository;
+using System.Data;
+using System.Configuration;
+using System.Data.OleDb;
+using BOTS_BL.Models;
+
+
+
+namespace AudioPalace.Controllers
+{
+   
+    public class LoginController : Controller
+    {
+        Exceptions newexception = new Exceptions();
+        AudioPalaceRepository APR = new AudioPalaceRepository();
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            LoginDetail objData = new LoginDetail();
+
+            return View(objData);
+        }
+
+        public ActionResult Authenticate(LoginDetail objData)
+        {
+            try
+            {
+                LoginDetail userDetails = new LoginDetail();
+                userDetails = APR.AuthenticateUser(objData);
+
+                if (userDetails != null)
+                {
+                    if (userDetails.LoginId != null)
+                    {
+                        Session["UserSession"] = userDetails;
+                        LoginDetail objLogData = new LoginDetail();
+                        objLogData.LoginId = userDetails.LoginId;
+                        objLogData.Password = userDetails.Password;
+                        //objLogData.LoggedInTime = DateTime.Now;
+                        //LR.AddLoginLog(objLogData);
+
+                        if (!string.IsNullOrEmpty(userDetails.LoginId))
+                        {
+                            return RedirectToAction("TransactionUpload", "Login");
+                        }
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(Convert.ToString(TempData["InvalidUserMessage"])))
+                        {
+                            TempData["InvalidUserMessage"] = "User Does not Exist";
+                        }
+                    }
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(Convert.ToString(TempData["InvalidUserMessage"])))
+                    {
+                        TempData["InvalidUserMessage"] = "User Does not Exist";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "");
+                TempData["InvalidUserMessage"] = ex.Message;
+                return RedirectToAction("Login");
+            }
+            return View("Login");
+        }
+
+
+        public ActionResult UploadProduct()
+        {
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    DataSet ds = new DataSet();
+                    HttpPostedFileBase files = Request.Files[0];
+                    string fileName = "ProductUpload_" + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsx";
+                    var path = ConfigurationManager.AppSettings["ProductFiles"].ToString();
+                    var fullFilePath = path + "\\" + fileName;
+                    files.SaveAs(path + "\\" + fileName);
+
+                    string conString = string.Empty;
+
+                    conString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fullFilePath + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1\"";
+
+                    using (OleDbConnection connExcel = new OleDbConnection(conString))
+                    {
+                        using (OleDbCommand cmdExcel = new OleDbCommand())
+                        {
+                            using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                            {
+                                cmdExcel.Connection = connExcel;
+                                //Get the name of First Sheet.
+                                connExcel.Open();
+                                DataTable dtExcelSchema;
+                                dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                                string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                                connExcel.Close();
+
+                                //Read Data from First Sheet.
+                                connExcel.Open();
+                                cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                                odaExcel.SelectCommand = cmdExcel;
+                                odaExcel.Fill(ds);
+                                connExcel.Close();
+
+                                
+                            }
+                            
+                        }
+                    }                                
+                    var ObjA = APR.BulkInsert(ds.Tables[0]);
+
+                    return new JsonResult() { Data = ObjA, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+                }
+                catch (Exception ex)
+                {
+                    newexception.AddException(ex, "UploadProduct");
+                }
+
+            }
+            return Json("File Not Uploaded Successfully!");
+
+        }
+
+        public ActionResult UploadTransaction()
+        {
+            //AudioPalace ObjA = new AudioPalace();
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    DataSet ds = new DataSet();
+                    HttpPostedFileBase files = Request.Files[0];
+                    string fileName = "TransactionUpload_" + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsx";
+                    var path = ConfigurationManager.AppSettings["TransactionFiles"].ToString();
+                    var fullFilePath = path + "\\" + fileName;
+                    files.SaveAs(path + "\\" + fileName);
+
+                    string conString = string.Empty;
+
+                    conString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fullFilePath + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1\"";
+
+                    using (OleDbConnection connExcel = new OleDbConnection(conString))
+                    {
+                        using (OleDbCommand cmdExcel = new OleDbCommand())
+                        {
+                            using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                            {
+                                cmdExcel.Connection = connExcel;
+                                //Get the name of First Sheet.
+                                connExcel.Open();
+                                DataTable dtExcelSchema;
+                                dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                                string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                                connExcel.Close();
+
+                                //Read Data from First Sheet.
+                                connExcel.Open();
+                                cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                                odaExcel.SelectCommand = cmdExcel;
+                                odaExcel.Fill(ds);
+                                connExcel.Close();
+
+                                //connExcel.Open();
+                                //cmdExcel.CommandText = "SELECT count(*) From [" + sheetName + "]";
+                                //odaExcel.SelectCommand = cmdExcel;
+                                //odaExcel.Fill(ds);
+                                //connExcel.Close();
+                            }
+                        }
+                    }
+
+
+                    var ObjA = APR.BulkTransaction(ds.Tables[0]);
+
+                    return new JsonResult() { Data = ObjA, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+                }
+                catch (Exception ex)
+                {
+                    newexception.AddException(ex, "UploadTransaction");
+                }
+
+            }
+            return Json("File Not Uploaded Successfully!");
+
+        }
+
+        
+
+        public ActionResult TransactionUpload()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ BOTS_BL/Repository/AudioPalaceRepository.cs
@@ -0,0 +1,146 @@
+﻿using BOTS_BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using BOTS_BL.Models;
+
+namespace BOTS_BL.Repository
+{
+    public class AudioPalaceRepository
+    {
+        Exceptions newexception = new Exceptions();
+
+        
+
+        public LoginDetail AuthenticateUser(LoginDetail objData)
+        {
+            //DatabaseDetail DBDetails = new DatabaseDetail();
+            LoginDetail userDetail = new LoginDetail();
+            using (var context = new BOTSDBContext())
+            {
+                userDetail = context.LoginDetails.Where(a => a.LoginId == objData.LoginId && a.Password == objData.Password).FirstOrDefault();
+
+            }
+            return userDetail;
+
+        }
+
+        public AudioPalace BulkInsert(DataTable dt)
+        {
+            AudioPalace Obj = new AudioPalace();
+            int c = 0;
+            Obj.Status = false;
+            try
+            {
+                Obj.TbleRWCount = Convert.ToString(dt.Rows.Count);
+
+                var conStr = ConfigurationManager.ConnectionStrings["BOTSDBContext"].ToString();
+
+                
+                //string dd = "*";
+                SqlCommand cmd = new SqlCommand();
+                SqlConnection con_del = new SqlConnection(conStr);
+                cmd.Connection = con_del;
+                cmd.CommandText = "DELETE FROM ProductMaster";
+                cmd.CommandTimeout = 80000;
+                con_del.Open();
+                cmd.ExecuteNonQuery();
+                con_del.Close();
+
+
+                SqlConnection con = new SqlConnection(conStr);
+                SqlBulkCopy objbulk = new SqlBulkCopy(con);
+                objbulk.DestinationTableName = "ProductMaster";
+
+                objbulk.ColumnMappings.Add("ProductCode", "ProductCode");
+                //objbulk.ColumnMappings.Add("CustId", "CustId");
+                objbulk.ColumnMappings.Add("ProductName", "ProductName");
+                objbulk.ColumnMappings.Add("CategoryCode", "CategoryCode");
+                //objbulk.ColumnMappings.Add("OutletId", "OutletId");
+                objbulk.ColumnMappings.Add("CategoryName", "CategoryName");
+                //objbulk.ColumnMappings.Add("Status", "Status");
+                objbulk.ColumnMappings.Add("SubCategoryCode", "SubCategoryCode");
+                objbulk.ColumnMappings.Add("SubCategoryName", "SubCategoryName");
+                
+
+                con.Open();
+                objbulk.WriteToServer(dt);
+                
+                con.Close();
+                Obj.Status = true;
+
+                
+            }
+            
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "BulkInsert");
+            }
+            c++;
+            Obj.DBInsertCount = Convert.ToString(c);
+            return Obj;
+        }
+
+
+        public AudioPalace BulkTransaction(DataTable dt)
+        {
+            AudioPalace Obj = new AudioPalace();
+            int c = 0;
+            Obj.Status = false;
+            try
+            {
+                Obj.TbleRWCount = Convert.ToString(dt.Rows.Count);
+
+                for (int i = 0; i < dt.Rows.Count; i ++)
+                {
+                    
+                    var conStr = ConfigurationManager.ConnectionStrings["BOTSDBContext"].ToString();
+                    using (var context = new BOTSDBContext(conStr))
+                    {
+                        SqlCommand cmd = new SqlCommand();
+                        SqlConnection Con = new SqlConnection(conStr);
+                        cmd.Connection = Con;
+                        cmd.CommandTimeout = 80000;
+
+                        cmd.Parameters.AddWithValue("@pi_OutletCode", Convert.ToString(dt.Rows[i]["OutletCode"]));
+                        cmd.Parameters.AddWithValue("@pi_MobileNo", Convert.ToString(dt.Rows[i]["MobileNo"]));
+                        cmd.Parameters.AddWithValue("@pi_TxnDateText", dt.Rows[i]["TxnDate"]);      
+                        cmd.Parameters.AddWithValue("@pi_CategoryCode", Convert.ToString(dt.Rows[i]["CategoryCode"]));
+                        cmd.Parameters.AddWithValue("@pi_SubCategoryCode", Convert.ToString(dt.Rows[i]["SubCategoryCode"]));
+                        cmd.Parameters.AddWithValue("@pi_ProductCode", Convert.ToString(dt.Rows[i]["ProductCode"]));
+                        cmd.Parameters.AddWithValue("@pi_InvoiceAmt", dt.Rows[i]["InvoiceAmount"]);
+                        Con.Open();
+                        cmd.CommandText = "sp_RetailAppTxnUpload";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        string _returnvalue = (string)cmd.ExecuteScalar();
+                        Con.Close();
+
+                        
+
+                        
+                        Obj.Status  = true;
+                        if(_returnvalue == "0")
+                        {
+                            c++;
+                        }
+
+                    }
+                    ;
+                }
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "BulkInsert");
+            }
+            Obj.DBInsertCount = Convert.ToString(c);
+            Obj.DBFailedCount = Convert.ToString(c);
+            return Obj;
+        }
+    }
+}
