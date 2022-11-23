5d62897bac256c623ed31ab770b932003de48cd0
sujith flowrin <sujithflowrin@gmail.com>
2022-11-23 15:21:13 +0530

    Campaign/Add JS file for Regex

---
+++ WebApp/Scripts/checktext.js
@@ -0,0 +1,13 @@
+﻿
+function CheckText(text) {
+    var val = text,status;
+    var english = /^[A-Za-z][A-Za-z0-9!$%&*()_+\-=\[\]{};':"\\|,.<>\/?\#\ \n\'']*@$/;
+    if (val != '') {
+        if (english.test(val) == false) {
+
+            status = 1;
+        }
+    }
+    return status;
+  }
+
