254345898870e1bce2aef51f684024ab4d9f5ac7
sujith flowrin <sujithflowrin@gmail.com>
2023-01-02 21:02:17 +0530

    RetailApp/ValidationMandatory

---
+++ RetailerApp/Scripts/ValidateMandatoryText.js
@@ -0,0 +1,29 @@
+﻿function ValidateMandatoryText() {
+    var C = Object.keys(object.objJsonData.JsonList1).length;// count of values in JsonList1
+
+    var Dynamicname = [], MandFieldText = [], status;
+    status = 1;
+    for (j = 0; j < C; j++) {
+        count = j + 1;
+
+        var innerCount = Object.keys(object.objJsonData.JsonList1[j]).length;
+        if (innerCount == 1) {
+
+            $.each(object.objJsonData.JsonList1[j], function (i, item) {
+
+                    Dynamicname[j] = item.FieldName
+                    if (item.FieldOptionId == "1") {
+                        var ctrlId1 = "txtCustome" + j;
+                        var Temp2 = $("#" + ctrlId1).val();
+                        MandFieldText[j] = item.FieldTypeId
+
+                        if (( MandFieldText[j] == "1") && ( Temp2 == "")) {
+                            status = Dynamicname[j];
+                            return status;
+                        }
+                    }
+            });
+        }
+    }
+    return status;
+}
---
+++ RetailerApp/Scripts/ValidateManditarySelect.js
@@ -0,0 +1,36 @@
+function ValidateManditorySelect(){
+  var C = Object.keys(object.objJsonData.JsonList1).length;// count of values in JsonList1
+   
+  var Dynamicname = [], MandFieldSelect = [],status;
+  status = true;
+  for (j = 0; j < C; j++) {
+            count = j + 1;
+
+            var ctrlId = "ddlCustomCtrl" + j;
+            var innerCount = Object.keys(object.objJsonData.JsonList1[j]).length;
+            if (innerCount > 1) {
+
+                var K = 1;
+                $.each(object.objJsonData.JsonList1[j], function (i, item) {
+                    if (K == 1) {
+
+                        var Temp1 = $("#" + ctrlId).val();
+
+                        Dynamicname[j] = item.FieldName;
+                        MandFieldSelect[j] = item.FieldTypeId
+                       
+                        if ((MandFieldSelect[j] == 1) && (Temp1 == 0))
+                       {
+                            status = Dynamicname[j];
+                            return status;
+                       }
+                    }
+                    K++;
+                });
+            }
+        }
+       return status;
+}
+
+
+        
