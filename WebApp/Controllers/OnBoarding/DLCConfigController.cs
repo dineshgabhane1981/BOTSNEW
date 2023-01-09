09ccebc0a32f8036623da0dd5cf0055b6108bf61
Dinesh <dinesh.gabhane@hotmail.com>
2023-01-09 18:14:48 +0530

    DLC Rehash Files added

---
+++ WebApp/Controllers/OnBoarding/DLCConfigController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers.OnBoarding
+{
+    public class DLCConfigController : Controller
+    {
+        // GET: DLCConfig
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
