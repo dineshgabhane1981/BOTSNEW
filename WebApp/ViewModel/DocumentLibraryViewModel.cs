424507647af5894da57188395999039fac79ce30
Samiksha Wanjari <samiksha.wanjari@outlook.com>
2023-03-16 16:56:23 +0530

    LibraryChanges

--- WebApp/ViewModel/DocumentLibraryViewModel.cs
+++ WebApp/ViewModel/DocumentLibraryViewModel.cs
@@ -12,5 +12,18 @@ namespace WebApp.ViewModel
         public List<SelectListItem> lstGroupDetails { get; set; }
 
         public tblGroupDetail tblGroupDetails { get; set; }
+        public string dept { get; set; }
+
+        public SelectListItem[] Departments()
+        {
+            return new SelectListItem[3] { new SelectListItem() { Text = "HR", Value = "HR" }, new SelectListItem() { Text = "Sales", Value = "Sales" }, new SelectListItem() { Text = "Finance", Value = "Finance" } };
+        }
+
+        public string DocType { get; set; }
+
+        public SelectListItem[] DocumentType()
+        {
+            return new SelectListItem[3] { new SelectListItem() { Text = "ConfigurationSheet", Value = "ConfigurationSheet" }, new SelectListItem() { Text = "KnowledgeSeries", Value = "KnowledgeSeries" }, new SelectListItem() { Text = "Creatives", Value = "Creatives" } };
+        }
     }
 }
