3fa5a2f3468fd9ea05419cac9f26e5171141915b
Samiksha Wanjari <samiksha.wanjari@outlook.com>
2023-04-26 12:11:21 +0530

    RedemptionRepository log changes

--- BOTS_BL/Repository/RedemptionRepository.cs
+++ BOTS_BL/Repository/RedemptionRepository.cs
@@ -11,29 +11,43 @@ namespace BOTS_BL.Repository
 {
     public class RedemptionRepository
     {
+        Exceptions newexception = new Exceptions();
         public List<GenerateOTPList> GenerateOTP(string Type, string CashIncentive, string Infrastructure, string Deposit, string Promotion)
         {
             List<GenerateOTPList> objData = new List<GenerateOTPList>();
-            
-            using (var context = new ChitaleDBContext())
+            try
             {
-                objData = context.Database.SqlQuery<GenerateOTPList>("sp_RedemptionOTP @pi_LoginId, @pi_CashIncentive, @pi_InfraStructure, @pi_Deposit, @pi_Promotion, @pi_Datetime,@pi_Type",
-                              new SqlParameter("@pi_LoginId", ""),
-                              new SqlParameter("@pi_CashIncentive", CashIncentive),
-                              new SqlParameter("@pi_InfraStructure", Infrastructure),
-                              new SqlParameter("@pi_Deposit", Deposit),
-                              new SqlParameter("@pi_Promotion", Promotion),
-                              new SqlParameter("@pi_Datetime", DateTime.Now.ToString("dd-MM-yyyy")),
-                              new SqlParameter("@pi_Type", Type)).ToList<GenerateOTPList>();
+                using (var context = new ChitaleDBContext())
+                {
+                    objData = context.Database.SqlQuery<GenerateOTPList>("sp_RedemptionOTP @pi_LoginId, @pi_CashIncentive, @pi_InfraStructure, @pi_Deposit, @pi_Promotion, @pi_Datetime,@pi_Type",
+                                  new SqlParameter("@pi_LoginId", ""),
+                                  new SqlParameter("@pi_CashIncentive", CashIncentive),
+                                  new SqlParameter("@pi_InfraStructure", Infrastructure),
+                                  new SqlParameter("@pi_Deposit", Deposit),
+                                  new SqlParameter("@pi_Promotion", Promotion),
+                                  new SqlParameter("@pi_Datetime", DateTime.Now.ToString("dd-MM-yyyy")),
+                                  new SqlParameter("@pi_Type", Type)).ToList<GenerateOTPList>();
+                }
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "GenerateOTP");
             }
             return objData;
         }
         public RedemptionValue GetRedeemptionData(string Type)
         {
             RedemptionValue objData = new RedemptionValue();
-            using (var context = new ChitaleDBContext())
+            try
             {
-                objData = context.RedemptionValues.Where(x => x.Status == "00" && x.Type == Type).FirstOrDefault();
+                using (var context = new ChitaleDBContext())
+                {
+                    objData = context.RedemptionValues.Where(x => x.Status == "00" && x.Type == Type).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "GetRedeemptionData");
             }
             return objData;
         }
@@ -41,19 +55,25 @@ namespace BOTS_BL.Repository
         public ChitaleSPResponse ValidateOTP(string Type, string CashIncentive, string Infrastructure, string Deposit, string Promotion,string OTP1, string OTP2)
         {
             ChitaleSPResponse objData = new ChitaleSPResponse();
-
-            using (var context = new ChitaleDBContext())
+            try
             {
-                objData = context.Database.SqlQuery<ChitaleSPResponse>("sp_RedemptionValues @pi_LoginId, @pi_CashIncentive, @pi_InfraStructure, @pi_Deposit, @pi_Promotion, @pi_Datetime,@pi_Type,@pi_OTPValue1,@pi_OTPValue2",
-                              new SqlParameter("@pi_LoginId", ""),
-                              new SqlParameter("@pi_CashIncentive", CashIncentive),
-                              new SqlParameter("@pi_InfraStructure", Infrastructure),
-                              new SqlParameter("@pi_Deposit", Deposit),
-                              new SqlParameter("@pi_Promotion", Promotion),
-                              new SqlParameter("@pi_Datetime", DateTime.Now.ToString("dd-MM-yyyy")),
-                              new SqlParameter("@pi_Type", Type),
-                              new SqlParameter("@pi_OTPValue1", OTP1),
-                              new SqlParameter("@pi_OTPValue2", OTP2)).FirstOrDefault<ChitaleSPResponse>();
+                using (var context = new ChitaleDBContext())
+                {
+                    objData = context.Database.SqlQuery<ChitaleSPResponse>("sp_RedemptionValues @pi_LoginId, @pi_CashIncentive, @pi_InfraStructure, @pi_Deposit, @pi_Promotion, @pi_Datetime,@pi_Type,@pi_OTPValue1,@pi_OTPValue2",
+                                  new SqlParameter("@pi_LoginId", ""),
+                                  new SqlParameter("@pi_CashIncentive", CashIncentive),
+                                  new SqlParameter("@pi_InfraStructure", Infrastructure),
+                                  new SqlParameter("@pi_Deposit", Deposit),
+                                  new SqlParameter("@pi_Promotion", Promotion),
+                                  new SqlParameter("@pi_Datetime", DateTime.Now.ToString("dd-MM-yyyy")),
+                                  new SqlParameter("@pi_Type", Type),
+                                  new SqlParameter("@pi_OTPValue1", OTP1),
+                                  new SqlParameter("@pi_OTPValue2", OTP2)).FirstOrDefault<ChitaleSPResponse>();
+                }
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "ValidateOTP");
             }
             return objData;
         }
