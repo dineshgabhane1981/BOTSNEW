843c0847aa587f98371bfc21c192b74bbc669d13
sujith flowrin <sujithflowrin@gmail.com>
2023-05-10 12:32:11 +0530

    Changes

--- BOTS_BL/Models/EventModule/EventModuleData.cs
+++ BOTS_BL/Models/EventModule/EventModuleData.cs
@@ -11,12 +11,12 @@ namespace BOTS_BL.Models.EventModule
         public string Mobileno { get; set; }
         public string CustomerName { get; set; }
         public string Gender { get; set; }
-        public DateTime  DOB { get; set; }
-        public DateTime AnniversaryDate { get; set; }
+        public DateTime?  DOB { get; set; }
+        public DateTime? AnniversaryDate { get; set; }
         public string Address { get; set; }
         public string EmailId { get; set; }
         public decimal Points { get; set; }
-        public DateTime LastTxnDate { get; set; }
+        public DateTime? LastTxnDate { get; set; }
         public DateTime? PointExp { get; set; }
     }
 }
