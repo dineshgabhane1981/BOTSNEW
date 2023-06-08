3b22fcc70347dd4fce05a9fc5e3f75bbe39300d6
sujith flowrin <sujithflowrin@gmail.com>
2023-06-08 07:39:55 +0530

    HeatMap/code for calling new sp

--- BOTS_BL/Repository/HeatMapsRepository.cs
+++ BOTS_BL/Repository/HeatMapsRepository.cs
@@ -14,18 +14,32 @@ namespace BOTS_BL.Repository
     public class HeatMapsRepository
     {
         //string connstr = CustomerConnString.ConnectionStringCustomer;
-
+        
         Exceptions newexception = new Exceptions();
+
         public List<DaywiseHourwise> GetDaywiseHourwiseData(string GroupId,string outletId, string connstr)
         {
             List<DaywiseHourwise> objDaywiseHourwise = new List<DaywiseHourwise>();
+            string DBName;
+            DBName = "MadhusudanTextiles_New";
             try
             {
-                using (var context = new BOTSDBContext(connstr))
+                if(GroupId == "1087")
                 {
-                    objDaywiseHourwise = context.Database.SqlQuery<DaywiseHourwise>("sp_BOTS_HeatMap @pi_GroupId, @pi_Date, @pi_LoginId, @pi_OutletId", new SqlParameter("@pi_GroupId", GroupId), new SqlParameter("@pi_Date", DateTime.Now.ToShortDateString()), new SqlParameter("@pi_LoginId", ""), new SqlParameter("@pi_OutletId", outletId)).ToList<DaywiseHourwise>();
-                    
+                    using (var context = new CommonDBContext())
+                    {
+                        objDaywiseHourwise = context.Database.SqlQuery<DaywiseHourwise>("sp_HeatMap @pi_GroupId, @pi_Date, @pi_LoginId, @pi_OutletId,@pi_DBName ", new SqlParameter("@pi_GroupId", GroupId), new SqlParameter("@pi_Date", DateTime.Now.ToShortDateString()), new SqlParameter("@pi_LoginId", ""), new SqlParameter("@pi_OutletId", outletId), new SqlParameter("@pi_DBName", DBName)).ToList<DaywiseHourwise>();
+                    }
                 }
+                else
+                {
+                    using (var context = new BOTSDBContext(connstr))
+                    {
+                        objDaywiseHourwise = context.Database.SqlQuery<DaywiseHourwise>("sp_BOTS_HeatMap @pi_GroupId, @pi_Date, @pi_LoginId, @pi_OutletId", new SqlParameter("@pi_GroupId", GroupId), new SqlParameter("@pi_Date", DateTime.Now.ToShortDateString()), new SqlParameter("@pi_LoginId", ""), new SqlParameter("@pi_OutletId", outletId)).ToList<DaywiseHourwise>();
+
+                    }
+                }
+                
             }
             catch (Exception ex)
             {
