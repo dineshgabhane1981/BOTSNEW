f6bc3d3ae7f7278e3bd89d3d1acff148e86590bb
sujith flowrin <sujithflowrin@gmail.com>
2023-07-18 18:59:35 +0530

    DLCReportingController

--- WebApp/Controllers/DLCReportingController.cs
+++ WebApp/Controllers/DLCReportingController.cs
@@ -23,13 +23,44 @@ namespace WebApp.Controllers
     public class DLCReportingController : Controller
     {
         DLCReportingRepository DLCR = new DLCReportingRepository();
-        Exception ex = new Exception();
+        
+        Exceptions newexception = new Exceptions();
         // GET: DLCReporting
         public ActionResult Index()
         {
             var userDetails = (CustomerLoginDetail)Session["UserSession"];
             var data = DLCR.GetDLCReportings(userDetails.GroupId, "0", "0", "4");
             return View(data);
+        }
+
+        [HttpPost]
+        public JsonResult GetCampaignSummary(string flag, string year, string month)
+        {
+            List<DLCReporting> data = new List<DLCReporting>();
+            try
+            {
+                var userDetails = (CustomerLoginDetail)Session["UserSession"];
+                data = DLCR.GetDLCReportings(userDetails.GroupId, month, year, flag);
+            }
+            catch (Exception ex)
+            {
+                newexception.AddException(ex, "GetCampaignSummary");
+            }
+            return new JsonResult() { Data = data, JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+        }
+
+        public ActionResult DLCNewReg()
+        {
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
+            var data = DLCR.GetDLCNew(userDetails.GroupId);
+            return View(data);
+        }
+
+        public ActionResult DLCDetail(string SourceId)
+        {
+            var userDetails = (CustomerLoginDetail)Session["UserSession"];
+            var data = DLCR.GetDLCDataDetail(userDetails.GroupId,SourceId);
+            return PartialView("_DLCNewDetail", data);
         }
     }
 }
