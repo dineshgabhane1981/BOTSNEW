586debe930d76899ee0a123886ec8aab2343b291
sujith flowrin <sujithflowrin@gmail.com>
2023-08-14 11:54:45 +0530

    ITOPSNew/ViewModel

---
+++ WebApp/ViewModel/ITOPSNewViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BOTS_BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.ViewModel
+{
+    public class ITOPSNewViewModel
+    {
+        public string GroupId { get; set; }
+        public List<CommonOTPDetails> ObjLstOTPDetails { get; set; }  
+        public string LoginLevelddl { get; set; }
+        public SelectListItem[] Levelddl()
+        {
+                return new SelectListItem[3] { new SelectListItem() { Text = "Group", Value = "1" }, new SelectListItem() { Text = "Brand", Value = "2" }, new SelectListItem() { Text = "Outlet", Value = "3" } };
+        }
+        
+
+    }
+}
