25f33906a0ab4772d3ffee568ea8de7350fb9ace
Dinesh <dinesh.gabhane@hotmail.com>
2024-06-11 17:00:27 +0530

    Communication in campaign

--- WebApp/ViewModel/NewCampaignViewModel.cs
+++ WebApp/ViewModel/NewCampaignViewModel.cs
@@ -13,6 +13,7 @@ namespace WebApp.ViewModel
         public string SMSVendor { get; set; }
         public string SMSBalance { get; set; }
         public List<SelectListItem> lstDataset { get; set; }
+
         public string dummyValue { get; set; }
 
         public SelectListItem[] BaseType()
@@ -22,6 +23,17 @@ namespace WebApp.ViewModel
         public SelectListItem[] LessAndGreater()
         {
             return new SelectListItem[4] { new SelectListItem() { Text = "Less Than", Value = "1" }, new SelectListItem() { Text = "Greater Than", Value = "2" }, new SelectListItem() { Text = "Equal To", Value = "3" }, new SelectListItem() { Text = "Points Range", Value = "4" } };
+        }
+        public SelectListItem[] CampaignSubtype()
+        {
+            return new SelectListItem[8] { new SelectListItem() { Text = "Only Festival Wish", Value = "Only Festival Wish" },
+                new SelectListItem() { Text = "Festival wish with a store offer", Value = "Festival wish with a store offer" },
+                new SelectListItem() { Text = "No offer, only communication", Value = "No offer, only communication" },
+                new SelectListItem() { Text = "Discount Offer", Value = "Discount Offer" },
+            new SelectListItem() { Text = "Free gift offer", Value = "Free gift offer" },
+            new SelectListItem() { Text = "Only redemption communication", Value = "Only redemption communication" },
+            new SelectListItem() { Text = "Only engagement", Value = "Only engagement" },
+            new SelectListItem() { Text = "Reminder about something", Value = "Reminder about something" }};
         }
     }
 }
