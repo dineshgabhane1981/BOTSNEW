c7ad6c905c07a090724a86fc58479b8b12663f83
Samiksha Wanjari <samiksha.wanjari@outlook.com>
2024-06-19 16:05:55 +0530

    Model changes

--- WebApp/ViewModel/ProgrammeViewModel.cs
+++ WebApp/ViewModel/ProgrammeViewModel.cs
@@ -20,6 +20,8 @@ namespace WebApp.ViewModel
         public List<SelectListItem> lstSMSWhatsAppScriptMaster { get; set; }
         public List<tblCampaignMaster> lstCampaigns { get; set; }
         public List<LisRules> lstRules { get; set; }
+
+        //public List<LisRules> lstRule { get; set; }
         public tblInActiveSMSWAScript tblInActiveSMSWAScript {get;set;}
         public tblInActiveSMSWAScript objInActiveSMSWAScript { get; set; }
         public tblBirthdaySMSWAScript tblBirthdaySMSWAScript { get; set; }
