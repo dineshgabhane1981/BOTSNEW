82b48d069458925dc48fe8c11bc34b8ab7b85416
Dinesh <dinesh.gabhane@hotmail.com>
2024-10-18 15:59:45 +0530

    Opt Out

--- DLC/Controllers/OptoutController.cs
+++ DLC/Controllers/OptoutController.cs
@@ -23,7 +23,21 @@ namespace DLC.Controllers
             DLCDashboardContent objData = new DLCDashboardContent();
             objData = DCR.GetDLCDashboardContent(sessionVariables.GroupId, sessionVariables.MobileNo);
             objData.MobileNo = sessionVariables.MobileNo;
-
+            string connStr = objCustRepo.GetCustomerConnString(sessionVariables.GroupId);
+            using (var context = new BOTSDBContext(connStr))
+            {
+                var earnPoint = context.tblCustPointsMasters.Where(x => x.MobileNo == sessionVariables.MobileNo && x.IsActive == true && x.PointsType == "Base").Sum(y => y.Points) ?? 0;
+                var BonousPoint = context.tblCustPointsMasters.Where(x => x.MobileNo == sessionVariables.MobileNo && x.IsActive == true && x.PointsType == "Bonus").Sum(y => y.Points) ?? 0;
+                var PointsToRS = context.tblRuleMasters.Select(x => x.PointsAllocation).FirstOrDefault();
+                var custumerDetails = context.tblCustDetailsMasters.Where(x => x.MobileNo == sessionVariables.MobileNo).FirstOrDefault();
+                var pointsinRs = (earnPoint + BonousPoint) * PointsToRS ?? 0;
+                pointsinRs = Math.Round(pointsinRs, 2);
+                ViewBag.earnPoint = earnPoint;
+                ViewBag.bonousPoint = BonousPoint;
+                ViewBag.pointsinRs = pointsinRs;
+                ViewBag.customerName = custumerDetails.Name;
+                ViewBag.optout = custumerDetails.DisableSMSWAPromo;
+            }
             return View(objData);
         }
         public ActionResult UpdateOptout(string IsOptout)
